bbba7ea0ae2388d171f0c5bc07f50a5fdb6755dc
igmanthony <igmanthony@gmail.com>
2017-01-02 17:33:24 -0600

    Transpose Broken, getting Name Function

--- User Scripts/Operation/2dmat_Transpose_v01.py
+++ User Scripts/Operation/2dmat_Transpose_v01.py
@@ -4,31 +4,38 @@ Takes a 2-dimensional matrix of size [m, n] and returns a transposed matrix of
 size [n, m].
 """
 
-from UserScript import UserOperation, ScriptIOData
+# from UserScript import UserOperation, ScriptIOData
+# from UserScriptSettingsObjects import DataSetSettingsObject as DSSO
+from UserScript import *
 import numpy as np
 
 class ds_user_script(UserOperation):
     """Creates a user script of the operation type."""
-
+    # Meta-data and information for the GUI
     name = 'Transpose'
     tooltip = 'Transposes a 2D matrix'
     nDimension = 2
     nDataSets = 1
     version = 0.1
 
-    def operation(self, DataOut, DataIn, Meta):
+    # Defines the input data parameter (the right clicked object here)
+    DataSet = DataSetSettingsObject(minimum=1, maximum=1, primaryEnabled=True)
+    DataSet.setDescription('An input Matrix of size [m, n]')
+
+    # Defines the settings toolbox that will pop up if there is more than 1
+    # setting.
+    settings = {'Input Matrix': DataSet}
+
+    def operation(self, DataOut, Meta):
         """The generic 'main' function of an operation type user script."""
-        # self.DataIn is a list of ScriptIOData types. We want the first (or
-        # slice [0]) of this list.
-        dataInputObject = DataIn[0]
+
+        dataInputObject = Meta['Input Matrix']
         # The first slice of this list contains a matrix attribute, that
         # should be a numpy array.
-        dataInputArray = dataInputObject.matrix
-        if not isinstance(dataInputArray, np.ndarray):
+        if not isinstance(dataInputObject, np.ndarray):
             raise TypeError('Is not an array!')
         else:
-            transposedArray = np.transpose(dataInputArray)
+            transposedArray = np.transpose(dataInputObject)
             outputObject = ScriptIOData()
             outputObject.matrix = transposedArray
             DataOut.append(outputObject)
-
